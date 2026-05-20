2eb3200792a245a9f06d1f1f1decd7a4fea300bc
parthivFarazi <parthivfarazi@icloud.com>
2026-05-19 23:46:07 -0400

    feat(analytics): add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import './index.css';
 import './components/panels/_registerImageSlot';
@@ -20,5 +21,6 @@ bootAudioOnFirstGesture();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 );
